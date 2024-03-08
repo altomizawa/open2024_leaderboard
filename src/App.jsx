62106f46ee0c871c30cf876bb258d9dcfb54df2f
altomizawa <alyssontomizawa@hotmail.com>
2024-03-08 16:53:42 -0300

    style: add sort icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import sortIcon from './assets/filter_list_FILL0_wght400_GRAD0_opsz24.svg'
 import './App.css'
 
 import LeaderboardItem from './components/LeaderboardItem/LeaderboardItem'
@@ -54,11 +55,11 @@ function App() {
       <div>
         <h1>Open 2024 LEADERBOARD</h1>
         <div className='leaderboard__header'>
-          <h2 style={{textAlign: 'left'}} onClick={handleNameSorting} >Nome</h2>
-          <h2 onClick={handleCategoryChange}>Categoria</h2>
-          <h2 onClick={handleFirstWodSorting}>24.1</h2>
-          <h2>24.2</h2>
-          <h2>24.3</h2>
+          <h2 style={{textAlign: 'left'}} onClick={handleNameSorting}>Nome<img src={sortIcon}/></h2>
+          <h2 onClick={handleCategoryChange}>Categoria <img src={sortIcon}/></h2>
+          <h2 onClick={handleFirstWodSorting}>24.1<img src={sortIcon}/></h2>
+          <h2>24.2<img src={sortIcon}/></h2>
+          <h2>24.3<img src={sortIcon}/></h2>
         </div>
         <CreateAthleteResult />
       </div>
