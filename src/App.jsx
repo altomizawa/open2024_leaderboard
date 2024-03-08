1207385ea70ce25260656735176d8595c1107d11
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-03-08 12:03:24 -0300

    24.1 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import LeaderboardItem from './components/LeaderboardItem/LeaderboardItem'
+
+import results from './database/results.json'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [filteredLeaderboard, setFilteredLeaderboard] = useState(results)
+  const [category, setCategory] = useState('RX')
+
+  
+  const filterLeaderboard = () => {
+    if (category === 'RX') {
+      setCategory('scaled')
+    } else {setCategory('RX')}
+    const filteredResults = results.filter(item => item.category === category);
+    setFilteredLeaderboard(filteredResults)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Open 2024 LEADERBOARD</h1>
+        <div className='leaderboard__header'>
+          <h2 style={{textAlign: 'left'}}>Nome</h2>
+          <h2 onClick={filterLeaderboard}>Categoria</h2>
+          <h2>24.1</h2>
+          <h2>24.2</h2>
+          <h2>24.3</h2>
+        </div>
+        {filteredLeaderboard.map((athlete) => <LeaderboardItem key={athlete.name} athlete={athlete} />)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/LeaderboardItem/LeaderboardItem.jsx
@@ -0,0 +1,24 @@
+export default function LeaderboardItem(props) {
+    const {name:name, category, firstWodTime:firstWodResult, firstWodReps, secondWodReps:secondWodResult, thirdWodReps: thirdWodResult} = props.athlete;
+
+    function formatTime(seconds) {
+        if(seconds === 900){
+            return firstWodReps
+        }
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    }
+
+    return (
+        <>
+        <div className='leaderboard__item'>
+          <p style={{textAlign: 'left'}}>{name}</p>
+          <p>{category}</p>
+          <p>{formatTime(firstWodResult)}</p>
+          <p>{secondWodResult}</p>
+          <p>{thirdWodResult}</p>
+        </div>
+        </>
+    )
+}
