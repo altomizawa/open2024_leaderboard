ad64413dc660f293ba184dd371628ac90bfd3a72
altomizawa <alyssontomizawa@hotmail.com>
2024-03-08 16:37:48 -0300

    feat: add name sorting ascending and descending

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ import results from './database/results.json'
 function App() {
   const [filteredLeaderboard, setFilteredLeaderboard] = useState(results.filter(item => item.category === 'RX'))
   const [category, setCategory] = useState('RX')
-  const [filteredResults, setFilteredResults] = useState([]);
+  const [ascendingNameOrder, setAscendingNameOrder] = useState(true)
+  // const [filteredResults, setFilteredResults] = useState([]);
 
-
+// HANDLE CATEGORY CHANGE
   const handleCategoryChange = () => {
     if (category === 'RX') {
       setFilteredLeaderboard(results.filter(item => item.category === 'scaled'))
@@ -23,21 +24,39 @@ function App() {
       setCategory('RX')
     }
   }
+// HANDLE NAME SORTING
+  const handleNameSorting = () => {
+      const nameFilter = filteredLeaderboard.filter(item => item.category === category);//filter by category first
+      ascendingNameOrder ? nameFilter.sort((a, b) => a.name.localeCompare(b.name)) : nameFilter.sort((b, a) => a.name.localeCompare(b.name)); // Sort by name alphabetically
+      setAscendingNameOrder(prevState => !prevState)
+      setFilteredLeaderboard(nameFilter)//render leaderboard
+  }
 
+// HANDLE FIRST WOD SORTING
+  const handleFirstWodSorting = () => {
+    const firstWodResults = filteredLeaderboard.sort((a,b) => a.firstWodTime - b.firstWodTime)
+    console.log(firstWodResults)
+    setFilteredLeaderboard(firstWodResults)
+  }
+
+
+// CREATE ATHLETE
   function CreateAthleteResult(){
     return (
       filteredLeaderboard.map((athlete) => <LeaderboardItem key={athlete.name} athlete={athlete} />)
     )
   }
+
+  
   
   return (
     <>
       <div>
         <h1>Open 2024 LEADERBOARD</h1>
         <div className='leaderboard__header'>
-          <h2 style={{textAlign: 'left'}}>Nome</h2>
+          <h2 style={{textAlign: 'left'}} onClick={handleNameSorting} >Nome</h2>
           <h2 onClick={handleCategoryChange}>Categoria</h2>
-          <h2>24.1</h2>
+          <h2 onClick={handleFirstWodSorting}>24.1</h2>
           <h2>24.2</h2>
           <h2>24.3</h2>
         </div>
