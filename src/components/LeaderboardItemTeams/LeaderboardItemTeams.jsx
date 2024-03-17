15bc41183f8b5ad81e964b99d1ec72e2690aae56
altomizawa <alyssontomizawa@hotmail.com>
2024-03-17 09:54:48 -0300

    feat: create team details

--- src/components/LeaderboardItemTeams/LeaderboardItemTeams.jsx
+++ src/components/LeaderboardItemTeams/LeaderboardItemTeams.jsx
@@ -3,8 +3,7 @@ import styles from './LeaderboardItemTeams.module.css'
 
 export default function LeaderboardItemTeams(props) {
     const[isAthletesOpen, setIsAthletesOpen] = useState(false);
-
-// console.log(props)
+    // GET
     const {team=team, athletes = team.athletes, ranking=ranking, totalScore } = props.team
 
     //Calculate points based on rankingWodOne+rankingWodTwo 
@@ -30,9 +29,33 @@ export default function LeaderboardItemTeams(props) {
         return total;
     }
 
+    // OPEN AND CLOSE TEAM DETAILS
     const handleTeamDetails = () => {
         setIsAthletesOpen(prevState => !prevState)
     }
+
+    // CREATE TEAM DETAILS
+    const CreateTeamDetails = (props) =>{
+        const {teamAthletes, index} = props;
+        console.log(teamAthletes)
+
+        return (
+            <>
+                {teamAthletes.map((athlete) => (
+                    <div key={athlete.name} className={styles.leaderboard__header}>
+                        <p style={{textAlign: 'left', color: 'grey'}}>{athlete.name}<span> ({athlete.rankingTotal})</span></p>
+                    </div>
+                ))}
+                             
+            </>
+        )
+    }
+
+    // CALCULATE TEAM TOTAL
+    const calculateTeamTotal = () => {
+        return calculateTeamWodOneResult() + calculateTeamWodTwoResult()
+    }
+
 
 
     // function formatTime(seconds) {
@@ -47,13 +70,15 @@ export default function LeaderboardItemTeams(props) {
     return (
         <>
         <div className={styles.leaderboard__item} style={{columnGap: '2rem'}}>
-          <p style={{textAlign: 'left'}}></p>
+          <p style={{textAlign: 'left'}}>{props.index+1}</p>
           <div>
             <p onClick={handleTeamDetails}style={{textAlign: 'left', fontWeight: 'bold', textDecoration: 'underline', cursor: 'pointer'}}>{team} <span>({ranking})</span></p>
-            {isAthletesOpen && athletes.map((athlete) => (<p style={{textAlign: 'left'}}>{athlete.name}</p>))}
+            {isAthletesOpen && <CreateTeamDetails teamAthletes={athletes} index={athletes.index} />}
           </div>
           <p>{calculateTeamWodOneResult()} pts</p>
           <p>{calculateTeamWodTwoResult()} pts</p>
+          <p>{calculateTeamWodThreeResult()} pts</p>
+          <p>{calculateTeamTotal()}</p>
           {/* <p>{calculateTeamWodThreeResult()}</p> */}
           {/* <p>{formatTime(firstWodResult)}</p> */}
           {/* <p>{secondWodResult}</p> */}
