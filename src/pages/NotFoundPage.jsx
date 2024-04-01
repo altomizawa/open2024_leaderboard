9b459e0b5972d3dbdf776d16299e02269bf00e34
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-04-01 19:24:07 -0300

    feat: create 404 page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function NotFoundPage() {
+  return(
+    <div style={{position: 'absolute', width: '100vw', height: '100vh', backgroundColor:'black', top: 0, left: 0, display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
+         <h1>Nothing to see here</h1>
+         <Link to='/' style={{textDecoration: 'underline'}}>Click here to go back</Link>
+    </div>
+  )
+}
