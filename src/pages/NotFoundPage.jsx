56af6503fdd3b25cb1d5147ac979c5a700f7ec6f
altomizawa <alyssontomizawa@hotmail.com>
2024-06-22 15:58:26 -0300

    style: change background color in not-found-page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function NotFoundPage() {
   return(
-    <div style={{position: 'absolute', width: '100vw', height: '100vh', backgroundColor:'black', top: 0, left: 0, display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{position: 'absolute', width: '100vw', height: '100vh', backgroundColor:'white', top: 0, left: 0, display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
          <h1>Nothing to see here</h1>
-         <Link to='/' style={{textDecoration: 'underline'}}>Click here to go back</Link>
+         <Link to='/' style={{textDecoration: 'underline', color: 'black', marginTop: '1rem'}}>Click here to go back</Link>
     </div>
   )
 }
